d40f3abda17bfb3e33a314c740fdc242e4668355
Sonu <sonuparmanik09@gmail.com>
2026-03-10 01:02:03 +0530

    #Day1 - Two Sum Problem

---
+++ LeetCode/Arrays/Easy/TwoSum.java
@@ -0,0 +1,28 @@
+/*
+Problem: Two Sum
+Platform: LeetCode
+Difficulty: Easy
+
+Approach:
+Used HashMap to store visited numbers.
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# SDE Interview Preparation
+
+Preparing for Software Engineer interviews.
+
+## Progress Tracker
+
+| Day | Topic | Problems |
+|----|------|------|
+| Day 1 | Arrays | 5 |
+| Day 2 | Binary Search | 4 |
+| Day 3 | Trees | 3 |
+| Day 4 | DBMS | Indexing |
+| Day 5 | OS | Deadlock |
+
+## Platforms
+- LeetCode
+- GeeksForGeeks
+
+## Language
+Java
